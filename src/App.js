f3df3004e95ea441f8859b1613c9bb3076de6dfd
ALI <ahmedaliou94@gmail.com>
2021-09-01 02:59:54 -0400

    React Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <p>Dashboard</p>
+      <p>Widget</p>
+      <p>Reviews</p>
+      <p>Customers</p>
+      <p>Online Analysis</p>
+      <p>Settings</p>
+    </div>
+  );
+}
+function Smallbox(props) {
+  return (
+    <div className='small-box'>
+      <p>{props.title}</p>
+      <h3>{props.data1}</h3>
+      <h3>{props.data2}</h3>
+      <h3>{props.data3}</h3>
+    </div>
+  );
+}
+function Maincontent(props) {
+  return (
+    <div className='main-content'>
+      <p>{props.title}</p>
+      <h3>{props.data}</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Sidebar />
+      <Smallbox title='Reviews' data1='1,281' />
+      <Smallbox title='Average Rating' data1='4.6' />
+      <Smallbox
+        title='Sentiment Analysis'
+        data1='960'
+        data2='122'
+        data3='321'
+      />
+      <Maincontent title='Website Visitors' data='821' />
     </div>
   );
 }
