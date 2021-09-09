57f626c7f89f9a03369bf9e9458bef1f334d1a44
ALI <ahmedaliou94@gmail.com>
2021-09-09 12:02:14 -0400

    student assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
- import App from './App';
+// import './index.css';
+import App from './App';
 import FormControlClass from './components/FormControlClass'
 import Nav from './components/NavRouter';
 import StudentScore from './components/student';
